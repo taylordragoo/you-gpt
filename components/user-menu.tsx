d227bae1cf0c690e9dde8453d48312e24466b202
nosyke <greatbeyondstudios@gmail.com>
2023-08-31 14:28:03 -0400

    Removed preview token

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -12,23 +12,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import { IconExternalLink } from '@/components/ui/icons'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle
-} from '@/components/ui/dialog'
-import { useState } from 'react'
-import {Input} from "@/components/ui/input";
-import {useLocalStorage} from "@/lib/hooks/use-local-storage";
 
 export interface UserMenuProps {
   user: Session['user']
 }
-
 
 function getUserInitials(name: string) {
   const [firstName, lastName] = name.split(' ')
@@ -36,15 +23,6 @@ function getUserInitials(name: string) {
 }
 
 export function UserMenu({ user }: UserMenuProps) {
-  const [previewToken, setPreviewToken] = useLocalStorage<string | null>(
-      'ai-token',
-      null
-  )
-
-  const token: string = JSON.parse(localStorage.getItem('ai-token') as string);
-  const [previewTokenDialog, setPreviewTokenDialog] = useState(false);
-  const [previewTokenInput, setPreviewTokenInput] = useState(previewToken ?? '')
-
   // @ts-ignore
   return (
     <div className="flex items-center justify-between">
@@ -72,12 +50,6 @@ export function UserMenu({ user }: UserMenuProps) {
           </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem
-            className="flex-col items-start"
-            onClick={() => setPreviewTokenDialog(true)}
-          >
-            Settings
-          </DropdownMenuItem>
-          <DropdownMenuItem
             onClick={() =>
               signOut({
                 callbackUrl: '/'
@@ -89,42 +61,6 @@ export function UserMenu({ user }: UserMenuProps) {
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-
-      <Dialog open={previewTokenDialog} onOpenChange={setPreviewTokenDialog}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Enter your OpenAI Key</DialogTitle>
-            <DialogDescription>
-              If you have not obtained your OpenAI API key, you can do so by{' '}
-              <a
-                  href="https://platform.openai.com/signup/"
-                  className="underline"
-              >
-                signing up
-              </a>{' '}
-              on the OpenAI website. This is only necessary for preview
-              environments so that the open source community can test the app.
-              The token will be saved to your browser&apos;s local storage under
-              the name <code className="font-mono">ai-token</code>.
-            </DialogDescription>
-          </DialogHeader>
-          <Input
-              value={previewTokenInput}
-              placeholder={token}
-              onChange={e => setPreviewTokenInput(e.target.value)}
-          />
-          <DialogFooter className="items-center">
-            <Button
-                onClick={() => {
-                  setPreviewToken(previewTokenInput)
-                  setPreviewTokenDialog(false)
-                }}
-            >
-              Save Token
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </div>
   )
 }
